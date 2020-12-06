aaeccd9eeabdddc83782601fc5a7093869317167
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2020-12-06 18:39:43 -0300

    add exception control with controllerAdvice

--- src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LibraryEventsController.class)
@@ -49,7 +50,33 @@ public class LibraryEventControllerUnitTest {
         //when
         mockMvc.perform(post("/v1/libraryevent").content(value).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
 
-        //then
+    }
+
+    @Test
+    void postLibraryEvent_4xx() throws Exception {
+        //given
+
+        Book book = Book.builder()
+                .bookId(null)
+                .bookAuthor(null)
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+
+        String expectedError = "book.bookAuthor-must not be blank, book.bookId-must not be null";
+        //when
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(value)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedError));
 
     }
 }
